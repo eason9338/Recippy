8034d05f51f98d919cbc5b550bcb6f89a533da4a
林姿慧 <n0923609898@yahoo.com.tw>
2024-06-05 22:27:35 +0800

    add some icons

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,6 +1,11 @@
 import { useUser } from '../context/UserContext.js';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShare } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faCommentMedical } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import '../style/Post.css';
 
@@ -9,6 +14,8 @@ const User = () => {
     const [posts, setPosts] = useState([]);
     const displayName = user && user.user_name ? user.user_name : '';
     const navigate = useNavigate();
+    const [likes, setLikes] = useState(0);
+
 
     const logout = () => {
         setUser(null)
@@ -36,42 +43,78 @@ const User = () => {
                     "post_id": 1,
                     "post_title": "Hello, World",
                     "post_content": "This is a test post",
-                    "post_tags": ["test", "hello"]
+                    "post_tags": ["test", "hello"],
+                    "like_tag": 0,
+                    "comment_tag": 0
                 },
                 {
                     "post_id": 2,
                     "post_title": "Goodbye, World",
                     "post_content": "This is another test post",
-                    "post_tags": ["test", "goodbye"]
+                    "post_tags": ["test", "goodbye"],
+                    "like_tag": 1,
+                    "comment_tag": 1
                 }
             ])
         }
         fetchPosts();
     }, [])
+    const likeClick = () => {
+        setLikes(likes + 1);
+      };
 
-    return ( 
+
+      return ( 
         <div>
             <h2 className={displayName ? '' : 'hide'}>Hello {displayName}, this is your personal page</h2>
             <div>
                 {
                     posts ? posts.map((post, index) => {
+                        console.log(post)
                         return (
                             <div key={index} className='post'>
-                                <h3>{post.post_title}</h3>
+                                <div class="">
+                                    <h3>{post.post_title}</h3>
+                                    <FontAwesomeIcon icon={faBars} />
+                                </div>
                                 <p>{post.post_content}</p>
                                 <div>
                                     {post.post_tags.map((tag, index) => {
-                                        return <span key={index}>{tag}</span>
+                                        return <span key={index} className='tag'>{tag}</span>
                                     })}
+                                    <div class="btn-group" role="group" aria-label="Basic outlined example">
+                                    {/* 讚數 */}
+                                        {/* <button  onClick={likeClick}  class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                                            </svg>likes {likes} */}
+                                    {/* 留言 */}
+                                        {/* </button>
+                                            <button type="button" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chat-square-heart" viewBox="0 0 16 16">
+                                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                                            <path d="M8 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
+                                            </svg>comment
+                                        </button> */}
+                                    {/* 分享 */}
+                                        {/* <button type="button" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+                                            </svg>share
+                                        </button> */}
+                                        <div class="button">
+                                            <FontAwesomeIcon icon={faHeart}onClick={likeClick}  className="like-icon" /> {likes} 
+                                            <FontAwesomeIcon icon={faCommentMedical} className="comment-icon" />       
+                                            <FontAwesomeIcon icon={faShare} style={{color: "#1f5120",}} />
+                                        </div>
+                                     </div>
                                 </div>
                             </div>
                         )
                     }) : ''
                 }
             </div>
+            <p>This is title page</p>
+            <p className={displayName ? '' : 'hide'}>Hello, {displayName}</p>
+            <button onClick={logout}>Log out</button>
         </div>
     );
 }
-
- 
 export default User;
